05e49546ee3c299d22de8bad7d0ebbdc9c979593
dssss12 <125500790+dssss12@users.noreply.github.com>
2023-12-19 19:32:02 +0300

    Initial

---
+++ Сорочкин/base/sample_postfix.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+#include <locale>
+#include "postfix.h"
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    string expression;
+    map<char, double> values;
+    double result;
+    cout << "Введите выражение с переменными (например, a+b*c): ";
+    cin >> expression;
+    TPostfix calculator(expression);
+    cout << "Выражение в постфиксной записи: " << calculator.GetPostfix() << endl;
+    vector<char> operands = calculator.GetOperands();
+    for (char op : operands) {
+        double value;
+        cout << "Введите значение для " << op << ": ";
+        cin >> value;
+        values[op] = value;
+    }
+    result = calculator.Calculate(values);
+    cout << "Результат: " << result << endl;
+    return 0;
+}
---
+++ Сорочкин/base/stack.h
@@ -0,0 +1,70 @@
+﻿#ifndef __STACK_H__
+#define __STACK_H__
+
+using namespace std;
+
+template <class T>
+class TStack {
+	int top;
+	size_t memSize;
+	T* pMem;
+public:
+	TStack()
+	{
+		top = -1;
+		memSize = 1;
+		pMem = new T[memSize];
+	}
+
+	T Pop()
+	{
+		if (top < 0)
+			throw "Stack is empty";
+		return pMem[top--];
+	}
+
+	T Peek() const
+	{
+		if (top < 0)
+			throw "Stack is empty";
+		return pMem[top];
+	}
+
+	void Push(const T& val)
+	{
+		if (top == memSize - 1)
+		{
+			memSize *= 2;
+			T* newMem = new T[memSize];
+			copy(pMem, pMem + top + 1, newMem);
+			delete[] pMem;
+			pMem = newMem;
+		}
+		pMem[++top] = val;
+	}
+
+	TStack(const TStack& other) : top(other.top), memSize(other.memSize), pMem(new T[other.memSize])
+	{
+		copy(other.pMem, other.pMem + top + 1, pMem);
+	}
+	TStack& operator=(TStack other)
+	{
+		swap(*this, other);
+		return *this;
+	}
+	friend void swap(TStack& first, TStack& second)
+	{
+		swap(first.top, second.top);
+		swap(first.memSize, second.memSize);
+		swap(first.pMem, second.pMem);
+	}
+
+
+	~TStack() { delete[] pMem; }
+
+	size_t getSize() const { return top + 1; }
+
+	bool IsEmpty() const { return top == -1; }
+};
+
+#endif
